f427f9f0ff0521baed1d68221c513237ba15d381
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-14 15:25:39 +0530

    achieved the diff minimum 2 points if Tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -18,11 +18,24 @@ do
 done
 
 if [ "$head" -eq 21 ] && [ "$tail" -eq 21 ]
-then
-	echo "Its a Tie"
+ then
+	if [ "$head" -eq 21 ]
+  	 then
+		while [ "$(($head-$tail))" -gt 2 ]
+		do
+			tail=$(($tail+1))
+		done
+	 else
+		while [ "$(($tail-$head))" -gt 2 ]
+		do
+			head=$(($head+1))
+		done
+	fi
 elif [ "$head" -eq 21 ]
-then
+ then
 	echo "Head won by" $(($head-$tail))
-else
+ else
 	echo "Tail won by" $(($tail-$head))
 fi
+echo "Final head value" $head;
+echo "Final tail count" $tail;

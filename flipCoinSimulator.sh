2e765e8911c6cfd21b407837a84102c36faf48f9
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-14 15:16:40 +0530

    Calculated Win or Tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,17 +4,25 @@ echo "WELCOME TO FLIP COIN SIMULATION PROBLEM"
 
 head=0;
 tail=0;
-read -p "Enter number of flip required" n
-for (( i=0; i<n; i++ ))
+
+while [ "$head" -lt 21 ] && [ "$tail" -lt 21 ]
 do
 
 	x=$(( RANDOM%2 ));
-	if [ $x -eq 1 ]
-	then
+	if [ $x -eq 1 ];
+	 then
          	head=$(($head+1))
-	else
+	 else
         	tail=$(($tail+1))
 	fi
 done
-echo "Number of head win" $head;
-echo "Number of tail win" $tail;
+
+if [ "$head" -eq 21 ] && [ "$tail" -eq 21 ]
+then
+	echo "Its a Tie"
+elif [ "$head" -eq 21 ]
+then
+	echo "Head won by" $(($head-$tail))
+else
+	echo "Tail won by" $(($tail-$head))
+fi

82e32c6013ed0cb0ebdfa5ae312d89dfb5be1902
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-14 14:43:53 +0530

    Basic flip coin using RANDOM

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "WELCOME TO FLIP COIN SIMULATION PROBLEM";
+
+x=$(( RANDOM%2 ));
+if [ $x -eq 1 ];
+ then
+        echo "Head is winner" ;
+ else
+        echo "Tail is winner" ;
+fi
